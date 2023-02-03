eaeaf46cef793b35069a9b8940b335a87f6e724d
Vladimir <vova_spirid@mail.ru>
2023-02-04 03:26:28 +0400

    pull request

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+    Console.WriteLine ("Введенное число является четным");
+else
+Console.WriteLine("Введенное число является нечетным");
